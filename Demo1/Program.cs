a3f57c5d670220f778f2badc8385a719404c2a2d
harckline <antoinecronier1@gmail.com>
2020-09-14 10:20:39 +0200

    Module1 demo

---
+++ Demo1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    public class Program
+    {
+        public static int test;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            Console.ReadKey();
+        }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set {
+                if (value > 5)
+                {
+                    myVar = value;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+        }
+
+        public int MyProperty1 { get; set; }
+
+        public void Test()
+        {
+            MyProperty = 10;
+            var test = MyProperty;
+        }
+    }
+}
